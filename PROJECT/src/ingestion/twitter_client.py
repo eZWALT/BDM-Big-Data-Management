dbaccc4a4f92727fe0019f8ecd9ca174e6de3d69
matzatezalo <mateja.zatezalo@estudiantat.upc.edu>
2025-03-19 15:55:21 +0100

    Introduced fetch replies (not tested so far)

--- PROJECT/src/ingestion/twitter_client.py
+++ PROJECT/src/ingestion/twitter_client.py
@@ -24,10 +24,7 @@ class MyStream(tweepy.StreamingClient):
 #     stream.filter()
 
 # Fetch tweets related to certain keyword
-def fetch_tweets(keyword, bearer_token):
-
-    # URL for recent tweets
-    url = "https://api.twitter.com/2/tweets/search/recent"
+def fetch_tweets(recent_url, keyword, bearer_token):
 
     # Specify tweet fields to be included in the response
     # can include 'lang' as in language as well etc.
@@ -36,11 +33,56 @@ def fetch_tweets(keyword, bearer_token):
     headers = {"Authorization": f"Bearer {bearer_token}"}
     params = {"query": keyword, "max_results": "10", "tweet_fields": tweet_fields}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(recent_url, headers=headers, params=params)
     if response.status_code == 200:
         return response.json()
     else:
         return response.status_code, response.text
+    
+"""
+Example JSON Response: (for me to know how it looks like)
+{
+  "data": [
+    {
+      "id": "1234567890",
+      "text": "Sample tweet text here...",
+      "created_at": "2021-07-01T12:00:00.000Z",
+      "public_metrics": {
+        "retweet_count": 100,
+        "reply_count": 50,
+        "like_count": 500
+      }
+    }
+  ],
+  "meta": {
+    "result_count": 1,
+    "newest_id": "1234567890",
+    "oldest_id": "1234567890",
+    "next_token": "b26v89c19zqg8o3fos5t9w8kd"
+  }
+}
+"""
+
+# Fetch replies to tweets to later analyze the sentiment 
+def fetch_replies(recent_url, tweet_id, author_id, bearer_token):
+
+    # Query for getting tweets referenced to original tweet (reply)
+    # tweet_id is the ID of original tweet
+    query = f"conversation_id:{tweet_id} to:{author_id}"
+    tweet_fields = "tweet.fields=created_at,author_id"
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    params = {
+        "query": query,
+        "tweet_fields": tweet_fields,
+        "max_results": "100"
+    }
+
+    response = requests.get(recent_url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(response.status_code, response.text)
+
     
 def main():
     # API keys and tokens
@@ -49,6 +91,9 @@ def main():
     bearer_token = 'AAAAAAAAAAAAAAAAAAAAAJ05zgEAAAAAXrxXMn8qfKBzpo7uKVrS6I0iPDM%3DctS3xKFHN5Uee9H544KiEuX7RQBhJFKqzgNlKk7hcPc18Noqta'
     access_token_secret = 'q7BMvNzxbBW4jeUvTFXBLd55TAvLuEldfxCQfRYAEVBpG'
     access_token = '1894441990378721280-2Nml1hRlQtKsG4JvZ0BOp86y6N1wim'
+
+    # URL for recent tweets
+    recent_url = "https://api.twitter.com/2/tweets/search/recent"
 
     # Authenticate to Twitter
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -60,7 +105,7 @@ def main():
     # setup_stream(auth, keywords)
     
     # Fetch historical data
-    tweets = fetch_tweets("Jordan", bearer_token)
+    tweets = fetch_tweets(recent_url, "Jordan", bearer_token)
     print(tweets)
 
 
