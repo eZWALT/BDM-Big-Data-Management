a4658ace080504a707aff6782e47a3445df06449
AimbotParce <marcparcerisa@gmail.com>
2025-03-31 01:44:40 +0200

    First version of kafka streaming

---
+++ PROJECT/src/ingestion/streaming/__init__.py
@@ -0,0 +1,231 @@
+import importlib
+import json
+import time
+from abc import ABC, abstractmethod
+from hashlib import sha256
+from multiprocessing import Process
+from re import finditer
+from typing import *
+
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+from loguru import logger
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    sys.path.append(root)
+    os.chdir(root)
+
+from src.utils.config import ConfigManager
+from src.utils.task import Task
+
+# ===-----------------------------------------------------------------------===#
+# Streaming Producing Tasks                                                    #
+#                                                                              #
+#                                                                              #
+# Author: Marc Parcerisa, Walter J.T.V                                         #
+# ===-----------------------------------------------------------------------===#
+
+
+# This producer is identified by ID
+class StreamProducer(ABC):
+    # To stream-produce in real time 60s per request is more than enough
+    def __init__(self, bootstrap_servers: str, topic: str):
+        self._producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=(lambda v: json.dumps(v).encode("utf-8")),
+        )
+        self._topic = topic
+
+    def send_message(self, message: dict):
+        self._producer.send(self._topic, value=message)
+
+    def produce_forever(self, query: str, *args, **kwargs):
+        """
+        Continuously produce data to the Kafka topic.
+        This method runs indefinitely, producing data based on the provided query.
+        It handles exceptions and restarts the producer if an error occurs.
+        """
+        matches = finditer(".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)", self.__class__.__name__)
+        cls_name = " ".join(m.group(0) for m in matches).upper()
+        while True:
+            try:
+                logger.info(f"[{cls_name}] Starting stream producer for query '{query}'...")
+                self.produce(query, *args, **kwargs)
+            except KeyboardInterrupt as e:
+                logger.info(f"[{cls_name}] Producer for query '{query}' interrupted. Exiting...")
+                self.close()
+                raise e
+            except Exception as e:
+                logger.warning(f"[{cls_name}] Error in producer for query '{query}': {e}. Restarting...")
+                time.sleep(5)  # Wait before restarting to avoid tight loop
+                continue
+            else:
+                logger.info(f"[{cls_name}] Producer for query '{query}' finished successfully.")
+                break
+        self.close()
+
+    @abstractmethod
+    def produce(self, query: str, *args, **kwargs):
+        """
+        Produce data continuously to the Kafka topic.
+        This method should be overridden by subclasses to implement specific
+        data production logic.
+
+        Children classes can make use of the send_message method to send messages
+        to the Kafka topic.
+        """
+        pass
+
+    def close(self):
+        self._producer.flush()
+        self._producer.close()
+
+    def __del__(self):
+        # Ensure the producer is closed and messages are flushed when the object is deleted
+        self.close()
+
+
+class DBConnectionConfig(TypedDict):
+    type: Literal["db"]
+    db_type: str
+    kwargs: Optional[dict]
+
+
+class ProducerConfig(TypedDict):
+    name: str
+    py_object: str
+    kwargs: Optional[dict]
+
+
+def _load_stream_producer(py_object: str) -> Type[StreamProducer]:
+    """
+    Load a batch producer class given a string in the following format:
+    <module>:<class_name>. The module must be in the PYTHONPATH.
+    """
+    module_name, class_name = py_object.split(":")
+    module = importlib.import_module(module_name)
+    cls = getattr(module, class_name)
+    if not isinstance(cls, type):
+        raise ValueError(f"Object {cls} is not a class")
+    return cls
+
+
+class StreamingProduceTask(Task):
+    def __init__(self):
+        super().__init__()
+        self.config = ConfigManager("config/stream.yaml")
+        self.producer_configs: List[ProducerConfig] = self.config._load_config()["producers"]
+        self.stream_producers: Dict[str, Tuple[Type[StreamProducer], dict]] = {}
+
+        for producer_config in self.producer_configs:
+            name = producer_config["name"]
+            if name in self.stream_producers:
+                raise ValueError(f"Duplicate producer name: {name}")
+            self.stream_producers[name] = (
+                _load_stream_producer(producer_config["py_object"]),
+                producer_config.get("kwargs", {}),
+            )
+
+        self.kafka_config = self.config._load_config()["kafka"]
+        self.kafka_admin = KafkaAdmin(bootstrap_servers=self.kafka_config["bootstrap_servers"])
+
+    def setup(self):
+        pass
+
+    @staticmethod
+    def _format_topic(producer_name: str, query: str) -> str:
+        """
+        Format the name of the task based on the producer name, database name, and query.
+        """
+        # Use sha256 to hash the query for a consistent length, and take the first 8 characters
+        query_hash = sha256(query.encode("utf-8")).hexdigest()[:8]
+        return f"{producer_name}-{query_hash}"
+
+    def execute(self, queries: List[str]):
+        processes: List[Process] = []
+        for name, (producer, kwargs) in self.stream_producers.items():
+            for query in queries:
+                topic = self._format_topic(name, query)
+                if not self.kafka_admin.topic_exists(topic):
+                    self.kafka_admin.create_topic(
+                        topic=topic,
+                        num_partitions=self.kafka_config["num_partitions"],
+                        replication_factor=self.kafka_config["replication_factor"],
+                    )
+                process = Process(
+                    target=producer(self.kafka_config["bootstrap_servers"], topic).produce_forever,
+                    args=(query),
+                    kwargs=kwargs,
+                    name=topic,
+                    daemon=True,
+                )
+                process.start()
+                processes.append(process)
+
+        for process in processes:
+            # Block indefinitely until someone kills the process
+            process.join()
+
+
+# ===-----------------------------------------------------------------------===#
+# Kafka Admin Setup                                                            #
+#                                                                              #
+# The KafkaAdmin class is a child class of KafkaAdminClient from the Kafka     #
+# library. It provides additional utility methods to manage Kafka topics,      #
+# such as checking if a topic exists (`topic_exists`), creating a new topic    #
+# (`create_topic`), and deleting topics (`delete_topics`). These methods       #
+# enhance the functionality of the base KafkaAdminClient class, making it      #
+# easier to manage Kafka topics in the StreamingProduceTask class.             #
+#                                                                              #
+# Author: Walter J.T.V, Marc Parcerisa                                         #
+# ===-----------------------------------------------------------------------===#
+
+
+class KafkaAdmin(KafkaAdminClient):
+    def topic_exists(self, topic: str) -> bool:
+        """
+        Check if a topic exists in the Kafka cluster.
+        """
+        try:
+            topics = self.list_topics()
+            return topic in topics
+        except Exception as e:
+            logger.error(f"Error checking if topic exists: {e}")
+            return False
+
+    def create_topic(self, topic: str, num_partitions: int, replication_factor: int):
+        """
+        Create a new topic in the Kafka cluster.
+        """
+        try:
+            if not self.topic_exists(topic):
+                new_topic = NewTopic(
+                    name=topic,
+                    num_partitions=num_partitions,
+                    replication_factor=replication_factor,
+                )
+                self.create_topics(new_topics=[new_topic])
+                logger.success(
+                    f"Created topic '{topic}' with {num_partitions} partitions and replication factor {replication_factor}."
+                )
+            else:
+                logger.warning(f"Topic '{topic}' already exists.")
+        except Exception as e:
+            logger.error(f"Error creating topic '{topic}': {e}")
+
+    def delete_topics(self, topics: List[str]):
+        try:
+            existing_topics = set(self.list_topics())
+            topics_to_delete = set(topics).intersection(existing_topics)
+            if not topics_to_delete:
+                logger.info("No topics to delete.")
+                return
+            super().delete_topics(topics_to_delete)
+            logger.success(f"Deleted topics: {topics_to_delete}")
+
+        except Exception as e:
+            logger.error(f"Error deleting topics: {e}")
---
+++ PROJECT/src/ingestion/streaming/youtube_stream.py
@@ -0,0 +1,92 @@
+import time
+from typing import List
+
+from loguru import logger
+
+from src.ingestion.connectors.models.youtube import VideoBasicData
+from src.ingestion.connectors.youtube_client import YoutubeAPIClient
+from src.ingestion.streaming import StreamProducer
+
+# ===-----------------------------------------------------------------------===#
+# Youtube Streaming Producer                                                   #
+#                                                                              #
+# This class represents a "Producer" for the Youtube API following the Kafka   #
+# Producer-Consumer and publication/subscription patterns. It continuously     #
+# polls the Youtube API for new videos matching a given query and sends them   #
+# to a Kafka topic.                                                            #
+#                                                                              #
+# Author: Marc Parcerisa                                                       #
+# ===-----------------------------------------------------------------------===#
+
+
+class YoutubeStreamProducer(StreamProducer):
+    def __init__(self, bootstrap_servers, topic):
+        super().__init__(bootstrap_servers, topic)
+        self._newest_video_id = None
+        self.client = YoutubeAPIClient()
+
+    def poll_new_videos(self, query: str, polling_size: int = 100) -> List[VideoBasicData]:
+        """
+        Poll the Youtube API for new videos matching the query.
+        This method retrieves the most recent videos and returns them as a list.
+        It also updates the internal state with the newest video ID.
+        """
+        if not self._newest_video_id:
+            # If no video ID is set, fetch the latest video
+            posts, cursor = self.client.retrieve_videos_basic_data(query=query, limit=1, order="date")  # Newest video
+            if posts:
+                self._newest_video_id = posts[0]["videoId"]
+            return []  # Don't return the first video, just set the ID, we'll return the next videos that are published
+        else:
+            # If a video ID is set, poll for new videos, and compare with the last one
+            posts, cursor = self.client.retrieve_videos_basic_data(query=query, limit=polling_size, order="date")
+            result = []
+            for post in posts:
+                if post["videoId"] == self._newest_video_id:
+                    break
+                result.append(post)
+            else:
+                # This will be executed if the loop is not broken, meaning we have more new videos
+                # than the batch size. This means that we're losing videos.
+                logger.warning(
+                    f"[YOUTUBE STREAM PRODUCER] Rate of publishing for query {query} is too high and is outpacing the "
+                    "polling rate. Consider increasing the batch size or the polling rate."
+                )
+
+            if result:
+                self._newest_video_id = result[0]["videoId"]
+            return result
+
+    def _fetch_and_update_video_stats(self, videos: List[VideoBasicData]):
+        """
+        Download the video stats for the given videos and update their dictionaries.
+        Perform the update in place.
+        """
+        stats = self.client.retrieve_video_statistics([video["videoId"] for video in videos])
+        for video in videos:
+            if video["videoId"] in stats:
+                video["statistics"] = stats[video["videoId"]]
+
+    def _fetch_and_update_video_captions(self, videos: List[VideoBasicData]):
+        """
+        Download the video captions (if available) for the given videos and update their dictionaries.
+        Perform the update in place.
+        """
+        for video in videos:
+            try:
+                captions_stream = self.client.get_caption_stream(video["videoId"])
+            except Exception as e:
+                continue
+            video["captions"] = captions_stream.generate_srt_captions()
+
+    def produce(self, query: str, polling_size: int = 100, polling_interval: int = 60):
+        while True:
+            videos = self.poll_new_videos(query, polling_size)
+            if videos:
+                self._fetch_and_update_video_stats(videos)
+                self._fetch_and_update_video_captions(videos)
+                for video in videos:
+                    self.send_message(video)
+            else:
+                logger.debug(f"No new videos found for query '{query}'.")
+            time.sleep(polling_interval)
