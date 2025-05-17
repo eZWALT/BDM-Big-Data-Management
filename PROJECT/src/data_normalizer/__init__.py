8d85cf45684558cd91fbfb6a21c7973d222f8635
AimbotParce <marcparcerisa@gmail.com>
2025-05-18 01:17:30 +0200

    Normalizer script done (I think)

---
+++ PROJECT/src/data_normalizer/__init__.py
@@ -0,0 +1,23 @@
+import os
+from typing import Dict, Optional, TypedDict
+
+from src.utils.config import ConfigManager
+
+
+class NormalizerConfig(TypedDict):
+    topics: Dict[str, str]  # Map from social network to topic name
+    conf: Optional[Dict[str, str]]  # Optional configuration for the normalizer
+    env_vars: Optional[Dict[str, str]]  # Optional environment variables for the normalizer
+    py_files: Optional[str]  # Optional Python files for the normalizer
+    output_dir: str  # Output directory for the normalizer
+
+
+def get_normalizer_config() -> NormalizerConfig:
+    """
+    Get the normalizer configuration for a given social network
+    """
+    config = ConfigManager(config_path="configuration/normalizer.yaml").config
+    return config
+
+
+normalizer_script = os.path.abspath(os.path.join(os.path.dirname(__file__), "data_normalizer.py"))
